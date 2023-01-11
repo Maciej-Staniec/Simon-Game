896db5036a847ea07a86fa316d9b71af6d31982a
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-11 06:45:32 +0000

    commit

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+// Variables
+let order = [];
+let playerOrder = [];
+let flash, turn, good, compTurn, intervalID;
+let strict = false;
+let noise = true;
+let on = false;
+let win;
+
+// Selectors
+const turnCounter = document.querySelector("#turn");
+const topLeft = document.querySelector("#topleft");
+const topRight = document.querySelector("#topright");
+const bottomLeft = document.querySelector("#bottomleft");
+const bottomRight = document.querySelector("#bottomright");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
+
+strictButton.addEventListener("change", () => {
+  if (strictButton.checked) {
+    strict = true;
+  } else {
+    strict = false;
+  }
+});
+
+onButton.addEventListener("change", () => {
+  if (onButton.checked) {
+    on = true;
+    turnCounter.textContent = "-";
+  } else {
+    on = false;
+    turnCounter.textContent = "";
+  }
+});
