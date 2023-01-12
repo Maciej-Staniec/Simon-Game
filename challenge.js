5894e5311af9aace0d7b11ec1a7bab9c4864d0a8
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-12 16:04:12 +0000

    commit

--- challenge.js
+++ challenge.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 // Selectors
 const startBtn = document.querySelector("#start");
 const powerBtn = document.querySelector("#on");
@@ -12,14 +12,15 @@ const blueBtn = document.querySelector("#bottomright");
 
 let isOn = false;
 let isStrict = false;
-let step, compPattern, playerPattern, flashColors, currentLevel;
-let compTurn = true;
+let step, compPattern, playerPattern, flashSteps, currentLevel, isGoodAnswer;
 let gameStarted = false;
-
+let win = false;
+let compTurn = true;
 // Listeners
 startBtn.addEventListener("click", () => {
-  if (isOn) {
+  if (isOn || win) {
     startGame();
+    clearColors();
   }
 });
 
@@ -30,10 +31,9 @@ powerBtn.addEventListener("change", () => {
   } else {
     isOn = false;
     countScreen.textContent = "";
-    clearInterval(flashColors);
-    clearColor();
+    clearInterval(flashSteps);
+    clearColors();
   }
-  console.log(isOn);
 });
 
 strictBtn.addEventListener("change", () => {
@@ -45,60 +45,88 @@ strictBtn.addEventListener("change", () => {
 });
 
 greenBtn.addEventListener("click", () => {
-  if (!compTurn) flash(greenBtn);
+  if (isOn && !compTurn) {
+    flash(greenBtn);
+    playerPattern.push(1);
+    console.log(`playerPattern.push(1), player pattern = ${playerPattern}`);
+    checkPlayerStep();
+  }
 });
 redBtn.addEventListener("click", () => {
-  if (!compTurn) flash(redBtn);
+  if (isOn && !compTurn) {
+    flash(redBtn);
+    playerPattern.push(2);
+    console.log(`playerPattern.push(2), player pattern = ${playerPattern}`);
+    checkPlayerStep();
+  }
 });
 yellowBtn.addEventListener("click", () => {
-  if (!compTurn) flash(yellowBtn);
+  if (isOn && !compTurn) {
+    flash(yellowBtn);
+    playerPattern.push(3);
+    console.log(`playerPattern.push(3), player pattern = ${playerPattern}`);
+    checkPlayerStep();
+  }
 });
 blueBtn.addEventListener("click", () => {
-  if (!compTurn) flash(blueBtn);
+  if (isOn && !compTurn) {
+    flash(blueBtn);
+    playerPattern.push(4);
+    console.log(`playerPattern.push(4), player pattern = ${playerPattern}`);
+    checkPlayerStep();
+  }
 });
 
 function flash(color) {
-  if (isOn && gameStarted) {
+  if (gameStarted) {
     if (color.id == "topleft") {
       greenBtn.style.backgroundColor = "lightgreen";
       let audio = document.getElementById("clip1");
       audio.play();
-      if (!compTurn) playerPattern.push(1);
+      if (!win) clearColors();
     }
     if (color.id == "topright") {
       redBtn.style.backgroundColor = "tomato";
       let audio = document.getElementById("clip2");
       audio.play();
-      if (!compTurn) playerPattern.push(2);
+      if (!win) clearColors();
     }
     if (color.id == "bottomleft") {
       yellowBtn.style.backgroundColor = "#ffff8e";
       let audio = document.getElementById("clip3");
       audio.play();
-      if (!compTurn) playerPattern.push(3);
+      if (!win) clearColors();
     }
     if (color.id == "bottomright") {
       blueBtn.style.backgroundColor = "lightblue";
       let audio = document.getElementById("clip4");
       audio.play();
-      if (!compTurn) playerPattern.push(4);
+      if (!win) clearColors();
     }
-    setTimeout(() => {
-      clearColor();
-    }, 200);
   }
 }
 
-function clearColor() {
-  greenBtn.style.backgroundColor = "darkgreen";
-  redBtn.style.backgroundColor = "darkred";
-  yellowBtn.style.backgroundColor = "#b3b300";
-  blueBtn.style.backgroundColor = "darkblue";
+function clearColors() {
+  setTimeout(() => {
+    greenBtn.style.backgroundColor = "darkgreen";
+    redBtn.style.backgroundColor = "darkred";
+    yellowBtn.style.backgroundColor = "#b3b300";
+    blueBtn.style.backgroundColor = "darkblue";
+  }, 400);
+}
+
+function flashColors() {
+  greenBtn.style.backgroundColor = "lightgreen";
+  redBtn.style.backgroundColor = "tomato";
+  yellowBtn.style.backgroundColor = "#ffff8e";
+  blueBtn.style.backgroundColor = "lightblue";
 }
 
 function startGame() {
+  isGoodAnswer = true;
   gameStarted = true;
   compTurn = true;
+  flashSteps = 0;
   currentLevel = 1;
   countScreen.textContent = currentLevel;
   step = 0;
@@ -108,26 +136,76 @@ function startGame() {
     compPattern.push(Math.trunc(Math.random() * 4) + 1);
   }
 
-  flashColors = setInterval(runPattern(), 800);
+  flashSteps = setInterval(runPattern, 800);
 }
 
 function runPattern() {
-  if (compTurn) {
+  isOn = false;
+  if (compTurn && gameStarted) {
     if (compPattern[step] === 1) flash(greenBtn);
     if (compPattern[step] === 2) flash(redBtn);
     if (compPattern[step] === 3) flash(yellowBtn);
     if (compPattern[step] === 4) flash(blueBtn);
     step++;
   }
+  console.log(`step = ${step}, currentLevel = ${currentLevel}`);
   if (step === currentLevel) {
-    setTimeout(() => {
-      compTurn = false;
-    }, 300);
+    isOn = true;
+    compTurn = false;
+    clearInterval(flashSteps);
   }
-
-  console.log(compTurn);
+  console.log(`compTurn = ${compTurn}, isOn = ${isOn}`);
 }
 
-// function checkPlayerStep() {
-//   if(player)
-// }
+function checkPlayerStep() {
+  if (
+    playerPattern[playerPattern.length - 1] !=
+    compPattern[playerPattern.length - 1]
+  ) {
+    console.log(
+      `playerPattern[step - 1] != compPattern[step - 1] = ${
+        playerPattern[step - 1] != compPattern[step - 1]
+      }`
+    );
+    console.log(playerPattern[step - 1], compPattern[step - 1]);
+    isGoodAnswer = false;
+  }
+
+  if (playerPattern.length === 3 && isGoodAnswer) {
+    won();
+  }
+
+  if (!isGoodAnswer) {
+    countScreen.textContent = "NO!";
+    setTimeout(() => {
+      startGame();
+    }, 800);
+    // compTurn = true;
+    // flashColors();
+    // setTimeout(() => {
+    //   playerPattern = [];
+    //   countScreen.textContent = currentLevel;
+    //   step = 0;
+    //   isGoodAnswer = true;
+    //   flashSteps = setInterval(runPattern(), 800);
+    // }, 1000);
+  }
+
+  if (playerPattern.length === currentLevel && isGoodAnswer && !win) {
+    step = 0;
+    playerPattern = [];
+    currentLevel++;
+    countScreen.textContent = currentLevel;
+    compTurn = true;
+    flashSteps = setInterval(runPattern, 800);
+  }
+}
+
+function won() {
+  countScreen.textContent = "WON!";
+  flashColors();
+  win = true;
+  isOn = false;
+  gameStarted = false;
+  clearInterval(flashSteps);
+}
