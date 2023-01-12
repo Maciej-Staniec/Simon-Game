422fe4979222ff7f43418a2cef7e54655dc724e4
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-01-12 12:35:25 +0000

    commit

--- challenge.js
+++ challenge.js
@@ -12,6 +12,9 @@ const blueBtn = document.querySelector("#bottomright");
 
 let isOn = false;
 let isStrict = false;
+let step, compPattern, playerPattern, flashColors, currentLevel;
+let compTurn = true;
+let gameStarted = false;
 
 // Listeners
 startBtn.addEventListener("click", () => {
@@ -27,6 +30,8 @@ powerBtn.addEventListener("change", () => {
   } else {
     isOn = false;
     countScreen.textContent = "";
+    clearInterval(flashColors);
+    clearColor();
   }
   console.log(isOn);
 });
@@ -39,4 +44,90 @@ strictBtn.addEventListener("change", () => {
   }
 });
 
-function startGame() {}
+greenBtn.addEventListener("click", () => {
+  if (!compTurn) flash(greenBtn);
+});
+redBtn.addEventListener("click", () => {
+  if (!compTurn) flash(redBtn);
+});
+yellowBtn.addEventListener("click", () => {
+  if (!compTurn) flash(yellowBtn);
+});
+blueBtn.addEventListener("click", () => {
+  if (!compTurn) flash(blueBtn);
+});
+
+function flash(color) {
+  if (isOn && gameStarted) {
+    if (color.id == "topleft") {
+      greenBtn.style.backgroundColor = "lightgreen";
+      let audio = document.getElementById("clip1");
+      audio.play();
+      if (!compTurn) playerPattern.push(1);
+    }
+    if (color.id == "topright") {
+      redBtn.style.backgroundColor = "tomato";
+      let audio = document.getElementById("clip2");
+      audio.play();
+      if (!compTurn) playerPattern.push(2);
+    }
+    if (color.id == "bottomleft") {
+      yellowBtn.style.backgroundColor = "#ffff8e";
+      let audio = document.getElementById("clip3");
+      audio.play();
+      if (!compTurn) playerPattern.push(3);
+    }
+    if (color.id == "bottomright") {
+      blueBtn.style.backgroundColor = "lightblue";
+      let audio = document.getElementById("clip4");
+      audio.play();
+      if (!compTurn) playerPattern.push(4);
+    }
+    setTimeout(() => {
+      clearColor();
+    }, 200);
+  }
+}
+
+function clearColor() {
+  greenBtn.style.backgroundColor = "darkgreen";
+  redBtn.style.backgroundColor = "darkred";
+  yellowBtn.style.backgroundColor = "#b3b300";
+  blueBtn.style.backgroundColor = "darkblue";
+}
+
+function startGame() {
+  gameStarted = true;
+  compTurn = true;
+  currentLevel = 1;
+  countScreen.textContent = currentLevel;
+  step = 0;
+  compPattern = [];
+  playerPattern = [];
+  for (let i = 0; i < 20; i++) {
+    compPattern.push(Math.trunc(Math.random() * 4) + 1);
+  }
+
+  flashColors = setInterval(runPattern(), 800);
+}
+
+function runPattern() {
+  if (compTurn) {
+    if (compPattern[step] === 1) flash(greenBtn);
+    if (compPattern[step] === 2) flash(redBtn);
+    if (compPattern[step] === 3) flash(yellowBtn);
+    if (compPattern[step] === 4) flash(blueBtn);
+    step++;
+  }
+  if (step === currentLevel) {
+    setTimeout(() => {
+      compTurn = false;
+    }, 300);
+  }
+
+  console.log(compTurn);
+}
+
+// function checkPlayerStep() {
+//   if(player)
+// }
